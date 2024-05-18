e3e131c676448d36e75f7030eac0a6f68d2b0f8b
rizkyymr <rizkyanakgaul123@gmail.com>
2024-05-18 14:28:34 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing:{
+        a: '.5em',
+        b: '-0.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
